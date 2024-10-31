5947ca46c12b023df6a3d545da484fbcfd5282ea
Abram-Emad <ArtistArtistAbram2015@outlook.com>
2024-10-31 17:00:09 +0300

    Uploding-react.js-loan-application-form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LoanFormComponnent from './LoanFormComponnent';
+
+function App() {
+  return (
+    <div className="App">
+      <LoanFormComponnent />
+    </div>
+  );
+}
+
+export default App;
